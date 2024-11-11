c6b3688d078dabc397d16d12080d4ecfe765ab7a
Ruben Cruz <acostakaku420@gmail.com>
2024-11-11 09:37:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
       <tbody>
         <?php
 
-        $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "PRUEBA");
+        $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('123456'), "PRUEBA");
 
         $cadenaSQL = "select * from s_cliente";
         $resultado = mysqli_query($conexion, $cadenaSQL);
